16ffb75d9317e80147762a0692bd6a54e4391fad
Platosdog <62915572+Platosdog@users.noreply.github.com>
2020-04-30 13:04:06 -0500

    Run after html load

--- site.js
+++ site.js
@@ -1,3 +1,7 @@
+window.addEventListener('load', (event) => {
+    console.log('page is fully loaded');
+  });
+
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(displayLocationInfo);
   }
